af538f7b105e7b122808b0380f69698a0d1ff3db
Thomas Royal <thomas.m.royal@gmail.com>
2012-08-14 10:03:16 -0400

    Initialized server commit with socket.io support with correct configuration options.

--- server.js
+++ server.js
@@ -3,3 +3,23 @@ var app = connect()
 	.use(connect.logger('dev'))
 	.use(connect.static('public'))
 	.listen(3000);
+var io = require('socket.io').listen(app);
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+io.set('log level', 1);                    // reduce logging
+io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
+
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
