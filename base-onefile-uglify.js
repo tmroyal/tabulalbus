86e5aa68d65ba16ce07aef8fefc6e0221131d6ee
Thomas Royal <thomas.m.royal@gmail.com>
2012-08-21 16:55:55 -0400

    Deleted old files.

--- base-onefile-uglify.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$("<div/>",{id:"button-bo"}).appendTo("body");$("#button-bo").css({top:"100px",left:"200px"})});
--- sugar/subclassOf.js
+++
@@ -1,5 +0,0 @@
-function subclassOf(base) {
-    _subclassOf.prototype= base.prototype;
-    return new _subclassOf();
-}
-function _subclassOf() {};
